3ac472619e78b68601bd71dc324a3ef54435cead
stan <stan@funnc.com>
2021-04-01 10:54:04 +0900

    아직도 세팅 중이다

---
+++ pages/_document.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+
+export default class todoDocument extends Document {
+  render() {
+    return (
+      <Html lang="ko">
+        <Head>
+          {/* 변하지 않는 head 또는 script 는 이곳에 넣는다. */}
+          {/* 예외 : 타이틀은 _app.js (nextjs 규칙) */}
+          <meta httpEquiv="X-UA-Compatible" content="IE=EDGE" />
+          {/* IE 캐시 방지 */}
+          <meta httpEquiv="Pragma" content="no-cache" />
+          <meta httpEquiv="Expires" content="-1" />
+          <meta name="format-detection" content="telephone=no" />
+          <link rel="shortcut icon" href="/favicon.ico" />
+          <title>todo Next</title>
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,8 @@
 import React from 'react';
-import Head from 'next/head';
 
 const Home = () => {
   return (
     <>
-      <Head>
-        <title>todo Next</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
       <div>테스트 생성</div>
     </>
   );
