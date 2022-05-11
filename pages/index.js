6bda58ec3201484bd4acb0d6fbfd9c111a4f5360
KyungHwanKim-devs <kkhdevs@gmail.com>
2022-05-11 15:58:53 +0900

    Sort order 기능 추가

--- pages/index.js
+++ pages/index.js
@@ -23,16 +23,20 @@ const TodoItem = ({ id, title, onClickDelete }) => (
 const Home = () => {
   const [text, setText] = useState('');
   const [todoList, setTodoList] = useState([]);
+  const [sortOrder, setSortOrder] = useState('descendente');
 
   useEffect(async () => {
-    try {
-      const { data } = await axios.get('/posts');
-      await Promise.resolve(sorting(data));
-      setTodoList(data);
-    } catch (e) {
-      console.error(e);
-    }
-  }, []);
+    const setTodoListFunc = async () => {
+      try {
+        const { data } = await axios.get('/posts');
+        await Promise.resolve(sorting(data, sortOrder));
+        setTodoList(data);
+      } catch (e) {
+        console.error(e);
+      }
+    };
+    setTodoListFunc();
+  }, [sortOrder]);
 
   const handleChange = (e) => {
     setText(e.target.value);
@@ -74,9 +78,21 @@ const Home = () => {
     }
   };
 
+  const handleSortOrder = () => {
+    sortOrder === 'descendente'
+      ? setSortOrder('asec')
+      : setSortOrder('descendente');
+  };
+
   return (
     <>
       <h1>todo list</h1>
+      <div>
+        <span>정렬 : </span>{' '}
+        <span onClick={() => handleSortOrder()}>
+          {sortOrder === 'asec' ? '오름차순' : '내림차순'}
+        </span>
+      </div>
       <input type="text" value={text} onChange={handleChange} />
       <button type="button" onClick={onClickAddBtn}>
         add
